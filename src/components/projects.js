42b65fbf89c17c3cefee8a6ad5edcb33b9979e09
ebbabw <ebbabergholtz@gmail.com>
2022-02-20 20:14:58 +0100

    newChanges

--- src/components/projects.js
+++ src/components/projects.js
@@ -9,21 +9,44 @@ export const Projects = () => {
 
     return (
 
+      <div>
+
+         <div className="copy">my work</div>
+
         <div className="intro-container"> 
                
            <div className="project">
-               <p>Code</p>
+           <img class="profile" src="/assets/pexels-negative-space-48606.jpg" alt="code"/>
+               <div class="overlay"></div>
+               <div class="copy-project">
+               <div class="title5">Code</div>
+               </div>
             </div>  
+
             <div className="project">
-               <p>Graphic design</p>
+            <img class="profile" src="/assets/song1.png" alt="gd"/>
+                <div class="overlay"></div>
+                <div class="copy-project">
+               <div class="title5">Graphic design</div>
+            </div>
+            </div> 
+
+            <div className="project">
+            <img class="profile" src="/assets/IMG_4924.jpg" alt="photog"/>
+                <div class="overlay"></div>
+                <div class="copy-project">
+                <div class="title5">Photography</div>
+            </div>
             </div>  
+
             <div className="project">
-               <p>Photograf</p>
-            </div>  
-            <div className="project">
-               <p>about</p>
+            <div class="overlay"></div>
+            <div class="copy-project">
+                <div class="title5">More about me</div>
+            </div>
             </div>    
   
+        </div>
         </div>
        
     )
